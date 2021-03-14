6ccb3565b197168e3ef7eae6c85d4a1e0ef3c7f6
anastasia <smolina.anastasia.14@gmail.com>
2021-03-14 08:57:20 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let absN = Math.abs(n);
+    let str = String(absN);
+    let result = str.split("").reverse().join("");
+    result = Number(result);
+    return result;
 }
